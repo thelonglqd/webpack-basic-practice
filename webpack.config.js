578c8486f07336645cf3dba12729453928d5a29f
thelonglqd <thelonglqd@gmail.com>
2019-08-28 15:57:43 +0700

    add cach busting content hash and add html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
+    filename: "main.[contentHash].js"
   },
   module: {
     rules: [
@@ -13,5 +15,9 @@ module.exports = {
         use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "src/template.html" }),
+    new CleanWebpackPlugin()
+  ]
 };
